79d5c8ba85111cc49b5103e6baa6ac94090ad8f9
sdgs2008091633 <69416984+sdgs2008091633@users.noreply.github.com>
2022-06-16 08:49:34 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,5 @@
-local queue=require("FIFO")
-local tmp=queue.new()
-
-tmp.ti("test")
- 
- 
-print(print(tmp.tr()) 
+--default fifo size is 65535
+local queue1=require("FIFO")
+local queue1=queue:new()
+queue1:ti("test")
+print(tqueue1:tr()) 
